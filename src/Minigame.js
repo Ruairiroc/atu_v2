cc3f8eb3a6e41f20dd4512cee8b7c87bce80e9ec
Ruairiroc <r.oconnor62@nuigalway.ie>
2024-10-18 16:52:22 +0100

    minigame adjustments

--- src/Minigame.js
+++ src/Minigame.js
@@ -13,10 +13,22 @@ function Minigame() {
                 width: "300px",
                 height: "200px",
                 backgroundColor: "#77aad2",
-                alignItems: "center",
-                justifyContent: "center"
+                margin: "auto",
+                paddingLeft:"250px",
+                paddingRight:"250px",
+                paddingBottom: "200px",
+                marginBottom: "20px"
             }}>
                 
+            </div>
+
+            <div style={{
+                width: "1000px",
+                height: "400px",
+                backgroundColor: "#77aad2",
+                margin: "auto",
+            }}>
+
             </div>
         </div>
     );
