0d89bbe34dd8b98c4beae958f8be85eb556fea3e
Ruairiroc <r.oconnor62@nuigalway.ie>
2024-10-18 15:50:07 +0100

    modified:   src/Minigame.js

--- src/Minigame.js
+++ src/Minigame.js
@@ -8,7 +8,16 @@ function Minigame() {
     return(
         <div>
             <h2>{sectionName}</h2>
-            <p style={{textAlign: "center", fontSize: "20px"}}>Select Section</p>
+
+            <div style={{
+                width: "300px",
+                height: "200px",
+                backgroundColor: "#77aad2",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                
+            </div>
         </div>
     );
 
